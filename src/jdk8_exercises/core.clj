75457e5c909a3418a2fb4ac8b26c8d4aa8164e1d
Tobias Bayer <tobayer@googlemail.com>
2014-02-06 08:35:50 +0100

    Print word list

--- src/jdk8_exercises/core.clj
+++ src/jdk8_exercises/core.clj
@@ -1,8 +1,17 @@
-(ns jdk8_exercises.core)
+(ns jdk8_exercises.core
+  (:use midje.sweet))
+
+(def word-list ["every" "problem" "in" "computer" "science"
+                "can" "be" "solved" "by" "adding" "another"
+                "level" "of" "indirection"])
 
 (def file-content
   (with-open [rdr (clojure.java.io/reader "resources/SonnetI.txt")]
     (doall (line-seq rdr))))
 
+(defn print-all []
+  (apply println word-list))
 
+(fact "prints out all the words"
+  (print-all))
 
