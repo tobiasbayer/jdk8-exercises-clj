bd53e65aa19b6d4ece1a365d15065c8d101fba99
Tobias Bayer <tobayer@googlemail.com>
2014-02-06 16:58:26 +0100

    Edit project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject jdk8-exercises "0.0.1-SNAPSHOT"
-  :description "Cool new project to do things and stuff"
+  :description "Solutions to the Lambda Programming Lab exercises in Clojure."
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[midje "1.6.0"]]}})
 
